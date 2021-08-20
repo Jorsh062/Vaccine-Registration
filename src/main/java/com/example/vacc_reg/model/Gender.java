a80683507a6d04e355d1ab4644c7d096aff05aa7
Jorsh062 <olajosh062@gmail.com>
2021-08-20 16:16:42 +0100

    another commit

--- src/main/java/com/example/vacc_reg/model/Gender.java
+++ src/main/java/com/example/vacc_reg/model/Gender.java
@@ -1,4 +1,4 @@
 package com.example.vacc_reg.model;
 
-public enum Gender { MALE, FEMALE, TRANSGENDER, BLACK, WHITE, LEGAL, CAUCASIANS, NONE
+public enum Gender { MALE, FEMALE, TRANSGENDER, BLACK, WHITE, LEGAL, CAUCASIANS, NONE, NEVER
 }
